ec27a8872bdba2e052d8e5471b03a07a99fd6c4f
HeideQueen <lucyfabz@gmail.com>
2021-05-27 02:42:28 -0500

    pages & routes

---
+++ src/pages/about.js
@@ -0,0 +1,29 @@
+import React from "react"
+
+function about() {
+  return (
+    <div>
+      <h1>About Page</h1>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Inventore
+        suscipit autem exercitationem, hic commodi ipsam tempora molestias
+        libero, possimus quia at consequuntur voluptates id, quisquam animi
+        consectetur illo blanditiis dolor?
+      </p>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Inventore
+        suscipit autem exercitationem, hic commodi ipsam tempora molestias
+        libero, possimus quia at consequuntur voluptates id, quisquam animi
+        consectetur illo blanditiis dolor?
+      </p>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Inventore
+        suscipit autem exercitationem, hic commodi ipsam tempora molestias
+        libero, possimus quia at consequuntur voluptates id, quisquam animi
+        consectetur illo blanditiis dolor?
+      </p>
+    </div>
+  )
+}
+
+export default about
