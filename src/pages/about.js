41ceba7861e49e3b56369b9e04f2acecc1a7b2e7
HeideQueen <lucyfabz@gmail.com>
2021-05-27 03:15:43 -0500

    creating a layout page

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,28 +1,31 @@
 import React from "react"
+import Layout from "../components/Layout"
 
 function about() {
   return (
-    <div>
-      <h1>About Page</h1>
-      <p>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Inventore
-        suscipit autem exercitationem, hic commodi ipsam tempora molestias
-        libero, possimus quia at consequuntur voluptates id, quisquam animi
-        consectetur illo blanditiis dolor?
-      </p>
-      <p>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Inventore
-        suscipit autem exercitationem, hic commodi ipsam tempora molestias
-        libero, possimus quia at consequuntur voluptates id, quisquam animi
-        consectetur illo blanditiis dolor?
-      </p>
-      <p>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Inventore
-        suscipit autem exercitationem, hic commodi ipsam tempora molestias
-        libero, possimus quia at consequuntur voluptates id, quisquam animi
-        consectetur illo blanditiis dolor?
-      </p>
-    </div>
+    <Layout>
+      <div>
+        <h1>About Page</h1>
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Inventore
+          suscipit autem exercitationem, hic commodi ipsam tempora molestias
+          libero, possimus quia at consequuntur voluptates id, quisquam animi
+          consectetur illo blanditiis dolor?
+        </p>
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Inventore
+          suscipit autem exercitationem, hic commodi ipsam tempora molestias
+          libero, possimus quia at consequuntur voluptates id, quisquam animi
+          consectetur illo blanditiis dolor?
+        </p>
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Inventore
+          suscipit autem exercitationem, hic commodi ipsam tempora molestias
+          libero, possimus quia at consequuntur voluptates id, quisquam animi
+          consectetur illo blanditiis dolor?
+        </p>
+      </div>
+    </Layout>
   )
 }
 
