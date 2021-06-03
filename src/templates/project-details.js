bb8e4d24865e0095186dc2421111e140d4669299
HeideQueen <lucyfabz@gmail.com>
2021-06-03 00:11:35 -0500

    template components

---
+++ src/templates/project-details.js
@@ -0,0 +1,20 @@
+import React from "react"
+import Layout from "../components/Layout"
+import { GatsbyImage, getImage } from "gatsby-plugin-image"
+
+import { details, featured, html } from "../styles/project-details.module.css"
+
+function ProjectDetails() {
+  return (
+    <Layout>
+      <div className={details}>
+        <h2>title</h2>
+        <h3>stack</h3>
+        <div className={featured}>{/* <GatsbyImage image={} /> */}</div>
+        {/* <div className={html} dangerouslySetInnerHTML={} /> */}
+      </div>
+    </Layout>
+  )
+}
+
+export default project - details
